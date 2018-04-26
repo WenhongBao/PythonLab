df5ef365533f05163110c530116ab4cfe86c8345
bzi1030765617 <bzi1030765617@gmail.com>
2018-04-26 09:02:21 +0100

    Finish GuessNumber

--- PythonApplication1/GuessNumber.py
+++ PythonApplication1/GuessNumber.py
@@ -1,1 +1,14 @@
-print('hello word')
+theNumber=input('Input the number and call another person to guess: ');
+go=True;
+
+while go:
+    guessNumber=input('Input the number you guess: ');
+    if guessNumber == theNumber:
+        print('Congratulations!!!!');
+        go=False;
+    elif guessNumber < theNumber:
+        print('Too small');
+    else:
+        print('Too big');
+else:
+    print('Finish');
